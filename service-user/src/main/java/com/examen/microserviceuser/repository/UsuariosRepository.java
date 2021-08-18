a2de489d3d0c97d21a16df2a2b509c0fd0ca48c9
Ebooks <davidunchiha@gmail.com>
2021-08-18 15:19:07 -0500

    examen-back

---
+++ service-user/src/main/java/com/examen/microserviceuser/repository/UsuariosRepository.java
@@ -0,0 +1,12 @@
+package com.examen.microserviceuser.repository;
+
+import com.examen.microserviceuser.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuariosRepository extends JpaRepository<Usuario,Long> {
+
+    Usuario findByCorreo(String correo);
+    void deleteByCorreo(String correo);
+}
